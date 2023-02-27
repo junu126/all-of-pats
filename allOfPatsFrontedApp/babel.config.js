d73f0a079dc241559d699799d9298e798214da38
junu126 <junukim.dev@gmail.com>
2023-02-27 19:14:25 +0900

    chore(yarn): fix build error

--- allOfPatsFrontedApp/babel.config.js
+++ allOfPatsFrontedApp/babel.config.js
@@ -1,20 +1,24 @@
-const baseConfig = require('../babel.config');
+// const baseConfig = require('../babel.config');
+
+// module.exports = {
+//   ...baseConfig,
+//   presets: [...baseConfig.presets, 'module:metro-react-native-babel-preset'],
+//   plugins: [
+//     ...baseConfig.plugins,
+//     [
+//       'module:react-native-dotenv',
+//       {
+//         moduleName: '@env',
+//         path: '.env',
+//         blacklist: null,
+//         whitelist: null,
+//         safe: true,
+//         allowUndefined: true,
+//       },
+//     ],
+//   ],
+// };
 
 module.exports = {
-  ...baseConfig,
-  presets: [...baseConfig.presets, 'module:metro-react-native-babel-preset'],
-  plugins: [
-    ...baseConfig.plugins,
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: true,
-        allowUndefined: true,
-      },
-    ],
-  ],
-};
+  "presets": ["module:metro-react-native-babel-preset"]
+}
